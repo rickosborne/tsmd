a14da4e814ef9a81423c142717c02de88989ba9c
Rick Osborne <ricko@rickosborne.org>
2025-08-31 10:20:22 -0700

    Add git info to published package.json

--- scripts/impl/repackage.ts
+++ scripts/impl/repackage.ts
@@ -1,5 +1,6 @@
-import { deepCopy } from "@rickosborne/foundation";
+import { deepCopy, deepSort } from "@rickosborne/foundation";
 import { entriesOf } from "@rickosborne/foundation/entries-of";
+import { gitInfo, type PackageJsonLike, preparePackageJsonForSerialization } from "@rickosborne/term";
 import { readFile, writeFile } from "node:fs/promises";
 import { join as pathJoin } from "path";
 
@@ -12,6 +13,7 @@ export const repackage = async (
 	srcDir: string,
 	distDir: string,
 ) => {
+	const git = gitInfo();
 	const projectPackage = JSON.parse(await readFile(pathJoin(projectRoot, "package.json"), "utf-8")) as PackageJson;
 	const packageTemplate = JSON.parse(await readFile(pathJoin(srcDir, "package-template.json"), "utf-8")) as PackageJson;
 	const outFile = pathJoin(distDir, "package.json");
@@ -35,5 +37,20 @@ export const repackage = async (
 		ex[`./${bareSpecifier}.cjs`] = `./${bareSpecifier}.cjs`;
 		ex[`./${bareSpecifier}.d.ts`] = `./${bareSpecifier}.d.ts`;
 	}
-	await writeFile(outFile, JSON.stringify(pkg, undefined, "\t"), "utf-8");
+	let commitLink: string | undefined = undefined;
+	if (typeof pkg.repository === "object" && (pkg.repository.url?.includes("http") ?? false)) {
+		commitLink = pkg.repository.url.replace("git+", "").replace(/\.git$/, "").concat("/commits/", git.commitHash);
+	}
+	pkg["git"] = {
+		authorName: git.authorName,
+		commitDateISO: git.commitDateISO,
+		commitHash: git.commitHash,
+		parentHash: git.parentHash,
+		...(git.sigKey != null ? { signingKeyId: git.sigKey.toLowerCase() } : {}),
+		...(git.tags.length === 1 ? { tag: git.tags[ 0 ] } : {}),
+		...(commitLink == null ? {} : { commitLink }),
+	};
+	const sorted = deepSort(pkg) as PackageJsonLike;
+	const corrected = preparePackageJsonForSerialization(sorted);
+	await writeFile(outFile, JSON.stringify(corrected, undefined, "\t"), "utf-8");
 };
